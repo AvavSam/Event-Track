521c8b4a3b18ea3f4bf304d190af0d349d579d07
AvavSam <avav2608005@gmail.com>
2025-03-02 11:00:19 +0800

    Add event routes and API resource for event management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -11,6 +12,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('events', [EventController::class, 'show'])
+  ->name('events');
+  Route::get('addEvent', function () {
+    return Inertia::render('add-event');
+  })->name('add-event');
+  Route::apiResource('api/v1/events', EventController::class);
 });
 
 require __DIR__.'/settings.php';
